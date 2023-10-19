fcef0f15a14d3285bffc8fc08cd43713779bf51e
donabe8898 <60559703+donabe8898@users.noreply.github.com>
2023-10-19 17:23:10 +0900

    PostgreSQLに入門しました。
    Signed-off-by: donabe8898 <60559703+donabe8898@users.noreply.github.com>

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # Imadesho
 Ticket制管理ツール for discord
+
+
+# 起動
+## Dockerでの立ち上げ
+1. `Imadesho/docker`で`sudo docker-compose up -d && docker-compose exec postgres bash`してコンテナに入る。postgresはユーザー名なのでお好みで変更可能。
+2. `su postgres`でユーザー切り替え
+3. `psql`で入れる。
+
+あとはテーブル作成など。
+
+参考: [RustでPostgreSQLに接続する](https://qiita.com/takisawa/items/4327c5cb33a8d28ff5e9)
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,40 @@
-fn main() {
-    println!("Hello, world!");
+use serde::{Deserialize, Serialize};
+use tokio;
+use tokio_postgres::{tls::TlsConnect, Client, Connection, Error, NoTls};
+
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+    let (client, conn) = tokio_postgres::Config::new()
+        .user("postgres")
+        .password("password")
+        .host("localhost")
+        .port(5432)
+        .dbname("testdb")
+        .connect(tokio_postgres::NoTls)
+        .await?;
+
+    // 接続タスク実行
+    tokio::spawn(async move {
+        if let Err(e) = conn.await {
+            eprintln!("connection err: {}", e);
+        }
+    });
+    println!("Connected to psql");
+
+    // レコードを取ってくる
+    let rows = client.query("select * from users", &[]).await?;
+
+    // 表示
+    for row in rows {
+        let id: i32 = row.get(0);
+        let name: &str = row.get(1);
+        println!("id: {}, name: {}", id, name);
+    }
+
+    Ok(())
 }
+
+/* 参考
+    - 【Rust】 Rust + PostgreSQL + tokio_postgresでDBアクセスする方法
+        - https://qiita.com/SakasuRobo/items/a72f916c1e1c8fb63de7
+*/
