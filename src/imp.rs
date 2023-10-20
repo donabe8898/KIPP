a9806f9de9a52da6a7b1f54ce808834bbc6075d2
donabe8898 <60559703+donabe8898@users.noreply.github.com>
2023-10-20 17:48:46 +0900

    discordからのdb接続OK
    Signed-off-by: donabe8898 <60559703+donabe8898@users.noreply.github.com>

---
+++ src/imp.rs
@@ -0,0 +1,52 @@
+use poise::serenity_prelude::{self as serenity, client, FutureExt};
+use serde::{Deserialize, Serialize};
+use tokio;
+use tokio_postgres::{
+    tls::{NoTlsStream, TlsConnect},
+    Client, Connection, Error, NoTls, Row, Socket,
+};
+// type Error = Box<dyn std::error::Error + Send + Sync>;
+pub struct Data {}
+type Context<'a> = poise::Context<'a, Data, Error>;
+
+// DB test command
+#[poise::command(slash_command)]
+pub async fn test(ctx: Context<'_>) -> Result<(), Error> {
+    /* 返答用string */
+    let mut response = String::new();
+    /* 接続実行 */
+    let (client, conn) = db_conn().await?;
+
+    // 接続タスク実行
+    tokio::spawn(async move {
+        if let Err(e) = conn.await {
+            eprintln!("connection err: {}", e);
+        }
+    });
+
+    /* DBテーブル取得 */
+    let rows = client.query("select * from users", &[]).await?;
+    // 表示とdiscord返信
+    for row in rows {
+        let id: i32 = row.get(0);
+        let name: &str = row.get(1);
+        println!("id: {}, name: {}", id, name);
+        response += &format!("id: {}, name: {}\n", id, name);
+    }
+    let _ = ctx.say(response).await;
+    Ok(())
+}
+
+/* データベース接続 */
+pub async fn db_conn() -> Result<(Client, Connection<Socket, NoTlsStream>), Error> {
+    let (client, conn) = tokio_postgres::Config::new()
+        .user("postgres")
+        .password("password")
+        .host("localhost")
+        .port(5432)
+        .dbname("testdb")
+        .connect(tokio_postgres::NoTls)
+        .await?;
+
+    Ok((client, conn))
+}
--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,38 @@
+mod imp;
+
+use poise::{framework, serenity_prelude as serenity};
 use serde::{Deserialize, Serialize};
+use std::env;
 use tokio;
 use tokio_postgres::{tls::TlsConnect, Client, Connection, Error, NoTls};
 
+// Poise用
+// strct.rsへ移動
+
+struct Data {}
+
 #[tokio::main]
 async fn main() -> Result<(), Error> {
-    let (client, conn) = tokio_postgres::Config::new()
-        .user("postgres")
-        .password("password")
-        .host("localhost")
-        .port(5432)
-        .dbname("testdb")
-        .connect(tokio_postgres::NoTls)
-        .await?;
-
-    // 接続タスク実行
-    tokio::spawn(async move {
-        if let Err(e) = conn.await {
-            eprintln!("connection err: {}", e);
-        }
-    });
-    println!("Connected to psql");
-
-    // レコードを取ってくる
-    let rows = client.query("select * from users", &[]).await?;
-
-    // 表示
-    for row in rows {
-        let id: i32 = row.get(0);
-        let name: &str = row.get(1);
-        println!("id: {}, name: {}", id, name);
-    }
-
+    /* Bot起動フェーズ */
+    dotenv::dotenv().ok();
+    env_logger::init();
+    let token = env::var("TOKEN").expect("missing get token");
+    let framework = poise::Framework::builder()
+        .options(poise::FrameworkOptions {
+            commands: vec![imp::test()],
+            ..Default::default()
+        })
+        .token(token)
+        .intents(serenity::GatewayIntents::non_privileged())
+        .setup(|ctx, _ready, framework| {
+            Box::pin(async move {
+                poise::builtins::register_globally(ctx, &framework.options().commands)
+                    .await
+                    .unwrap();
+                Ok(imp::Data {})
+            })
+        });
+    framework.run().await.unwrap();
     Ok(())
 }
 
