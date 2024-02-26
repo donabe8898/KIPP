dd80a93a0b7f3cda086f53692b9dfb8770eca0bf
donabe8898 <60559703+donabe8898@users.noreply.github.com>
2024-02-26 14:26:03 +0900

    DocComment auth.rs
    Signed-off-by: donabe8898 <60559703+donabe8898@users.noreply.github.com>

--- src/auth.rs
+++ src/auth.rs
@@ -1,7 +1,18 @@
+//! 認証系の実装
+
 use poise::serenity_prelude::{self as serenity, GuildId};
 use std::env;
 
-// guild_id: ctxのguildID
+/// ギルドIDを比較するメソッド
+///
+/// # 引数
+///
+/// * `guild_id` - ギルドID
+///
+/// スラッシュコマンドを使用した場所のギルドIDが引数に渡される.
+/// Botの読み取る.envファイルのギルドIDと異なる場合はエラーを返す.
+///
+/// これはBotが他のサーバーに招待されDBの中身（タスク）を見られることを防ぐ目的がある.
 pub fn auth(guild_id: GuildId) -> Result<(), serenity::Error> {
     // .envからギルドIDとってくる
     let auth_guild = env::var("GUILD_ID").expect("missing get token");
